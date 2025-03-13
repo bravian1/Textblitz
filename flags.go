092c1ea245744ebc723cd4a9d14a9aa41483b047
Wambita <wambitafana@gmail.com>
2025-03-13 13:05:41 +0300

    Feat: define  cmd  line flags

--- flags.go
+++ flags.go
@@ -14,6 +14,15 @@ type CLIFlags struct {
 //Parseflags parses command line arguments and returns a CLIFlags struct
 func ParseFlags() CLIFlags {
 	//flags
-	command := flag.String("c", "", "Command: 'index' to index a file, ")
+	command := flag.String("c", "", "Command: 'index' to index a file, 'lookup to search a hash ")
+	inputFile := flag.String("i", "", "Input file(text file for  index, .idx for  lookup)")
+    chunkSize := flag.Int("s", 1024, "Chunk size in bytes (default 4096)")
+	outputFile := flag.String("o", "", "Output index file (.idx) .Required for 'index' command")
+	SimHash := flag.String("h", "", "Simhash value to search (required for 'lookup' command)")
+	help := flag.Bool("help", false, "Display help message")
+
+//parse flags
+flag.Parse()
+
 }
 
