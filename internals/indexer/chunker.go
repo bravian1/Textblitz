d52e061b904bb5876a217803a97a4457fd62d4d7
jmuchiri <mmjames451@gmail.com>
2025-03-14 23:54:03 +0300

    feat: change function flow to read pdf files separately

--- internals/indexer/chunker.go
+++ internals/indexer/chunker.go
@@ -1,11 +1,39 @@
 package indexer
 
 import (
+	"bytes"
+	"errors"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"sync"
 )
 
+// Chunk divides a file into chunks of specified size
+// Supports .txt, .pdf, and .docx files
 func Chunk(filename string, chunkSize int) ([][]byte, error) {
+	// Get file extension
+	ext := strings.ToLower(filepath.Ext(filename))
+
+	// Process based on file type
+	switch ext {
+	case ".txt":
+		return chunkFileWithGoroutines(filename, chunkSize)
+	case ".pdf":
+		data, err := extractPDFTextWithGoPdf(filename)
+		if err != nil {
+			return nil, err
+		}
+		return chunkDataWithGoroutines(data, chunkSize)
+
+	default:
+		return nil, errors.New("unsupported file type: " + ext)
+	}
+}
+
+// chunkFileWithGoroutines chunks a file using goroutines
+func chunkFileWithGoroutines(filename string, chunkSize int) ([][]byte, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
@@ -36,4 +64,44 @@ func Chunk(filename string, chunkSize int) ([][]byte, error) {
 
 	wg.Wait()
 	return chunks, nil
+}
+
+// chunkDataWithGoroutines chunks a byte slice using goroutines
+func chunkDataWithGoroutines(data []byte, chunkSize int) ([][]byte, error) {
+	dataLen := len(data)
+	numChunks := (dataLen + chunkSize - 1) / chunkSize
+
+	chunks := make([][]byte, numChunks)
+	var wg sync.WaitGroup
+	wg.Add(numChunks)
+
+	for i := 0; i < numChunks; i++ {
+		go func(i int) {
+			defer wg.Done()
+			start := i * chunkSize
+			end := start + chunkSize
+			if end > dataLen {
+				end = dataLen
+			}
+			chunks[i] = make([]byte, end-start)
+			copy(chunks[i], data[start:end])
+		}(i)
+	}
+
+	wg.Wait()
+	return chunks, nil
+}
+
+// extractPDFTextWithGoPdf extracts text from a PDF file using an alternative free library
+func extractPDFTextWithGoPdf(filename string) ([]byte, error) {
+	// Run pdftotext command (requires poppler-utils to be installed)
+	cmd := exec.Command("pdftotext", filename, "-")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	return out.Bytes(), nil
 }
